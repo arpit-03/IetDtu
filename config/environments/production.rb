abc43817fc0b13a4b24cc5ebfeb6db0579664648
arpit <arpitsachdeva03@gmail.com>
2019-02-03 13:27:56 +0530

    capistrano2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.assets.compile = false
   config.cache_classes = true
-  config.require_master_key = true
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
